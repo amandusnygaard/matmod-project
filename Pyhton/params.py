2e7f983774a6cd8e80671e2afa950406ebe0931b
amandusnygaard <98781227+amandusnygaard@users.noreply.github.com>
2022-11-23 19:30:02 +0100

    Add files via upload

---
+++ Pyhton/params.py
@@ -0,0 +1,10 @@
+from scipy.constants import pi
+
+#physical constants
+r_c = 0.22e-6                          #[m]
+h_c = 15e-9                            #[m]
+alpha = 8e-7                           #[m2 s^-1]
+k_on = 4e3                             #[(mol/m3)^-1 s^-1]
+k_off = 5                              #[s-1]
+
+phys_param = [r_c,h_c,alpha,k_on,k_off]
